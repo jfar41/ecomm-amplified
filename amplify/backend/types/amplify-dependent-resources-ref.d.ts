1175afbaa0f63b263d2c6a6fdd75ece7b5211de2
jfar41 <68749301+jfar41@users.noreply.github.com>
2022-10-04 12:07:41 -0700

    transition legacy GraphQL API to V2

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "ecommamplified": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
